1fc576f041dd10678b006c502df705440487ce47
Dan Wells <dantheradarman@gmail.com>
2022-04-07 17:46:34 +0100

    🔧 Server test command now only runs server-side tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  globalSetup: '<rootDir>/test/globalSetup.ts'
+  globalSetup: '<rootDir>/test/globalSetup.ts',
+  roots: [
+    "./src"
+  ]
 };
