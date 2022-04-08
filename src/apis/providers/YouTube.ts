357ff71473aa808b52c646e2be9bbc43d3e3e591
Dan Wells <dantheradarman@gmail.com>
2022-04-08 23:30:53 +0100

    🆕 Added /me/playlists endpoint Refactored auth for YouTube to match Spotify implementation

--- src/apis/providers/YouTube.ts
+++ src/apis/providers/YouTube.ts
@@ -1,5 +1,6 @@
 import { google, youtube_v3 } from 'googleapis';
 import { duration } from 'moment';
+import googleAuthProvider from '../../auth/providers/Google2';
 import { config } from '../../config';
 import { AuthAccount, AuthProvider } from '../../entities/AuthAccount';
 import { User } from '../../entities/User';
@@ -39,14 +40,56 @@ export class YouTubeAPI extends ExternalAPI {
     return data;
   }
 
-  formatPlaylist(playlist: youtube_v3.Schema$SearchResult) {
-    return {
+  formatPlaylist(playlist: youtube_v3.Schema$SearchResult | youtube_v3.Schema$Playlist) {
+    if (typeof playlist.id === 'string') {
+      const playlistTyped = playlist as youtube_v3.Schema$Playlist;
+      const formatted: ExternalPlaylist = {
+        provider: MediaProvider.YouTube,
+        providerId: playlistTyped.id ?? '',
+        name: playlistTyped.snippet?.title ?? '',
+        description: playlistTyped.snippet?.description ?? '',
+        image: playlistTyped.snippet?.thumbnails?.default?.url ?? '',
+      };
+      return formatted;
+    }
+    const playlistTyped = playlist as youtube_v3.Schema$SearchResult;
+    const formatted: ExternalPlaylist = {
       provider: MediaProvider.YouTube,
-      providerId: playlist.id?.playlistId,
-      name: playlist.snippet?.title,
-      description: playlist.snippet?.description,
-      image: playlist.snippet?.thumbnails?.default?.url,
-    } as ExternalPlaylist;
+      providerId: playlistTyped.id?.playlistId ?? '',
+      name: playlistTyped.snippet?.title ?? '',
+      description: playlistTyped.snippet?.description ?? '',
+      image: playlistTyped.snippet?.thumbnails?.default?.url ?? '',
+    };
+    return formatted;
+  }
+
+  /**
+   * Runs a youtube API function as the given auth user.
+   * Refreshes their access token if needed.
+   * @param authAccount The auth account to run as
+   * @param fn The spotify API function to run
+   * @returns The result from the API function
+   */
+  private async runAsAuthAccount<T extends (
+    ...args: any) => any>(
+    authAccount: AuthAccount | undefined,
+    fn: T,
+  )
+    : Promise<ReturnType<T>> {
+    try {
+      const auth = this.makeAuthParam(authAccount);
+      const result = await fn(auth);
+      return result;
+    } catch (e: any) {
+      console.log(e);
+      if (e.response.status === 401 && authAccount) {
+        await googleAuthProvider.refreshTokens(authAccount);
+        const auth = this.makeAuthParam(authAccount);
+        const result = await fn(auth);
+        return result;
+      }
+    }
+    throw new Error('Could not run as auth user');
   }
 
   /**
@@ -71,12 +114,14 @@ export class YouTubeAPI extends ExternalAPI {
 
   async getTracks(ids: string[], user?: User): Promise<ExternalTrack[]> {
     const authAccount = await this.tryGetUserAuthAccount(user);
-    const authParam = this.makeAuthParam(authAccount);
-    const result = await api.videos.list({
-      part: ['snippet', 'contentDetails'],
-      id: ids,
-      ...authParam,
-    });
+    const result = await this.runAsAuthAccount(
+      authAccount,
+      (authParam) => api.videos.list({
+        part: ['snippet', 'contentDetails'],
+        id: ids,
+        ...authParam,
+      }),
+    );
     if (result.data.items && result.data.items.length > 0) {
       return result.data.items.map((video) => this.formatTrack(video));
     }
@@ -85,14 +130,16 @@ export class YouTubeAPI extends ExternalAPI {
 
   async searchTracks(params: TrackSearchParams, user?: User): Promise<ExternalTrack[]> {
     const authAccount = await this.tryGetUserAuthAccount(user);
-    const authParam = this.makeAuthParam(authAccount);
-    const result = await api.search.list({
-      part: ['snippet'],
-      q: params.q,
-      maxResults: 10,
-      type: ['video'],
-      ...authParam,
-    });
+    const result = await this.runAsAuthAccount(
+      authAccount,
+      (authParam) => api.search.list({
+        part: ['snippet'],
+        q: params.q,
+        maxResults: 10,
+        type: ['video'],
+        ...authParam,
+      }),
+    );
     if (result.data.items && result.data.items.length > 0) {
       const ids = result.data.items.map((video) => video.id?.videoId as string);
       return this.getTracks(ids, user);
@@ -102,14 +149,16 @@ export class YouTubeAPI extends ExternalAPI {
 
   async searchPlaylists(params: SearchParams, user?: User): Promise<ExternalPlaylist[]> {
     const authAccount = await this.tryGetUserAuthAccount(user);
-    const authParam = this.makeAuthParam(authAccount);
-    const result = await api.search.list({
-      part: ['snippet'],
-      q: params.q,
-      maxResults: 10,
-      type: ['playlist'],
-      ...authParam,
-    });
+    const result = await this.runAsAuthAccount(
+      authAccount,
+      (authParam) => api.search.list({
+        part: ['snippet'],
+        q: params.q,
+        maxResults: 10,
+        type: ['playlist'],
+        ...authParam,
+      }),
+    );
     if (result.data.items && result.data.items.length > 0) {
       const playlists = result.data.items.map((playlist) => this.formatPlaylist(playlist));
       return playlists;
@@ -118,19 +167,19 @@ export class YouTubeAPI extends ExternalAPI {
   }
 
   async getMyPlaylists(params: PaginationParams, user: User): Promise<ExternalPlaylist[]> {
-    // const authAccount = await this.tryGetUserAuthAccount(user);
-    // if (!authAccount) { throw new Error('No account for this user'); }
-    // const authParam = this.makeAuthParam(authAccount);
-    // const result = await api.playlists.list({
-    //   part: ['snippet', 'contentDetails'],
-    //   maxResults: 50,
-    //   mine: true,
-    //   ...authParam,
-    // });
-    // if (result.data.items && result.data.items.length > 0) {
-    //   const playlists = result.data.items.map((playlist) => this.formatPlaylist(playlist));
-    //   return playlists;
-    // }
+    const authAccount = await this.tryGetUserAuthAccount(user);
+    if (!authAccount) { throw new Error('No account for this user'); }
+    const authParam = this.makeAuthParam(authAccount);
+    const result = await api.playlists.list({
+      part: ['snippet', 'contentDetails'],
+      maxResults: 50,
+      mine: true,
+      ...authParam,
+    });
+    if (result.data.items && result.data.items.length > 0) {
+      const playlists = result.data.items.map((playlist) => this.formatPlaylist(playlist));
+      return playlists;
+    }
     return [];
   }
 }
