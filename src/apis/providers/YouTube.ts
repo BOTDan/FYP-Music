b82e21557d250f8b90a9473e016ce56e63c0488a
Dan Wells <dantheradarman@gmail.com>
2022-04-09 03:32:56 +0100

    🕷 Fixed YouTube tokens not always getting refreshed

--- src/apis/providers/YouTube.ts
+++ src/apis/providers/YouTube.ts
@@ -81,8 +81,7 @@ export class YouTubeAPI extends ExternalAPI {
       const result = await fn(auth);
       return result;
     } catch (e: any) {
-      console.log(e);
-      if (e.response.status === 401 && authAccount) {
+      if (e.code === 401 && authAccount) {
         await googleAuthProvider.refreshTokens(authAccount);
         const auth = this.makeAuthParam(authAccount);
         const result = await fn(auth);
@@ -206,13 +205,15 @@ export class YouTubeAPI extends ExternalAPI {
   async getMyPlaylists(params: PaginationParams, user: User): Promise<ExternalPlaylist[]> {
     const authAccount = await this.tryGetUserAuthAccount(user);
     if (!authAccount) { throw new Error('No account for this user'); }
-    const authParam = this.makeAuthParam(authAccount);
-    const result = await api.playlists.list({
-      part: ['snippet', 'contentDetails'],
-      maxResults: 50,
-      mine: true,
-      ...authParam,
-    });
+    const result = await this.runAsAuthAccount(
+      authAccount,
+      (authParam) => api.playlists.list({
+        part: ['snippet', 'contentDetails'],
+        maxResults: 50,
+        mine: true,
+        ...authParam,
+      }),
+    );
     if (result.data.items && result.data.items.length > 0) {
       const playlists = result.data.items.map((playlist) => this.formatPlaylist(playlist));
       return playlists;
