2057e3d14a4bc06f6deb827e3a77859b19ba3c3e
Dan Wells <dantheradarman@gmail.com>
2022-04-09 03:33:55 +0100

    📝 TopHeading can now display an image

--- client/src/components/structure/TopHeading.tsx
+++ client/src/components/structure/TopHeading.tsx
@@ -1,15 +1,27 @@
 import React, { PropsWithChildren, ReactNode } from 'react';
+import { SquareImage } from './SquareImage';
 import './TopHeading.scss';
 
 export interface TopHeadingProps extends PropsWithChildren<{}> {
   subheading: string | ReactNode | undefined;
+  image?: string;
 }
 
-export function TopHeading({ children, subheading }: TopHeadingProps) {
+export function TopHeading({ children, subheading, image }: TopHeadingProps) {
+  const imageComponent = image
+    ? <SquareImage className="TopHeading__Image" src={image} />
+    : undefined;
   return (
     <div className="TopHeading">
-      {subheading && <p className="TopHeading__Subheading">{ subheading }</p>}
-      <h1 className="TopHeading__Heading">{ children }</h1>
+      {imageComponent}
+      <div className="TopHeading__Content">
+        {subheading && <p className="TopHeading__Subheading">{ subheading }</p>}
+        <h1 className="TopHeading__Heading">{ children }</h1>
+      </div>
     </div>
   );
 }
+
+TopHeading.defaultProps = {
+  image: undefined,
+};
