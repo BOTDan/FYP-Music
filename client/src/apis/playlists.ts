689765536f180a427adac64ec76664af6d72e169
Dan Wells <dantheradarman@gmail.com>
2022-04-09 02:03:41 +0100

    🆕 Hooked up playlist API to frontend

--- client/src/apis/playlists.ts
+++ client/src/apis/playlists.ts
@@ -1,11 +1,37 @@
 import { authFetch } from '.';
 import { ExternalPlaylist, MediaProvider, UserTokenDTO } from '../types';
 
+/**
+ * Queries the API to search for playlists
+ * @param provider The media provider to use
+ * @param token The user token
+ * @returns A list of playlists
+ */
 export async function getProviderPlaylists(
   provider: MediaProvider,
   token: UserTokenDTO,
 ): Promise<ExternalPlaylist[]> {
   const response = await authFetch(`/api/${provider.toLowerCase()}/me/playlists`, token);
+  if (!response.ok) {
+    throw new Error('Response not OK');
+  }
+  const data = await response.json();
+  return data;
+}
+
+/**
+ * Queries the API for a playlist
+ * @param provider The media provider to use
+ * @param id The id of the playlist
+ * @param token The user token
+ * @returns A playlist
+ */
+export async function getProviderPlaylist(
+  provider: MediaProvider,
+  id: string,
+  token: UserTokenDTO,
+): Promise<ExternalPlaylist> {
+  const response = await authFetch(`/api/${provider.toLowerCase()}/playlists/${encodeURIComponent(id)}`, token);
   if (!response.ok) {
     throw new Error('Response not OK');
   }
--- client/src/pages/playlists/SinglePlaylistPage.tsx
+++ client/src/pages/playlists/SinglePlaylistPage.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { getProviderPlaylist } from '../../apis/playlists';
+import { TrackCardList } from '../../components/cards/lists/TrackCardList';
+import { LoadingSpinner } from '../../components/icons/LoadingSpinner';
 import { ProviderIcon } from '../../components/icons/ProviderIcon';
 import { GeneralContent } from '../../components/layout/GeneralContent';
 import { TopHeading } from '../../components/structure/TopHeading';
 import { mediaProviderFromString } from '../../helper';
+import { useAppSelector } from '../../store/helper';
+import { ExternalPlaylist, UserTokenDTO } from '../../types';
 
 export interface SinglePlaylistPageProps {
   provider: string;
@@ -11,24 +16,74 @@ export interface SinglePlaylistPageProps {
 
 export function SinglePlaylistPage({ provider, id }: SinglePlaylistPageProps) {
   const finalProvider = mediaProviderFromString(provider);
+  const [playlist, setPlaylist] = useState<ExternalPlaylist | undefined>(undefined);
+  const userToken = useAppSelector((state) => state.auth.token);
+  const isMounted = useRef(false);
+  const [loading, setLoading] = useState(false);
 
-  if (finalProvider) {
-    return (
-      <GeneralContent className="PlaylistsPageResults" padTop padBottom>
+  const updatePlaylist = (
+    newProvider: string,
+    newId: string,
+    newUserToken: UserTokenDTO | undefined,
+  ) => {
+    const finalNewProvider = mediaProviderFromString(newProvider);
+    setPlaylist(undefined);
+
+    if (finalNewProvider && newUserToken) {
+      setLoading(true);
+
+      getProviderPlaylist(finalNewProvider, newId, newUserToken)
+        .then((r) => {
+          if (isMounted.current) {
+            console.log(r);
+            setLoading(false);
+            setPlaylist(r);
+          }
+        })
+        .catch((e) => {
+          setLoading(false);
+          console.log(e);
+        });
+    }
+  };
+
+  useEffect(() => {
+    isMounted.current = true;
+
+    return () => {
+      isMounted.current = false;
+    };
+  });
+
+  useEffect(() => {
+    updatePlaylist(provider, id, userToken);
+  }, [provider, id, userToken]);
+
+  let content = <p>Init</p>;
+
+  if (finalProvider && playlist) {
+    content = (
+      <>
         <TopHeading subheading={(
           <span>
             <ProviderIcon provider={finalProvider} /> Playlist
           </span>
         )}
         >
-          {id}
+          {playlist.name}
         </TopHeading>
-      </GeneralContent>
+        <TrackCardList tracks={playlist.tracks ?? []} />
+      </>
     );
   }
+
+  if (loading) {
+    content = (<LoadingSpinner size="5x" />);
+  }
+
   return (
-    <GeneralContent className="PlaylistsPageResults">
-      <TopHeading subheading="Playlists">{id}</TopHeading>
+    <GeneralContent className="SinglePlaylistPage" padTop padBottom>
+      {content}
     </GeneralContent>
   );
 }
