46af232c92e249e0941a31d004927ddd3011f3d8
Dan Wells <dantheradarman@gmail.com>
2022-04-10 22:33:23 +0100

    🆕 Added create playlist popup

--- client/src/pages/playlists/PlaylistsPageLanding.tsx
+++ client/src/pages/playlists/PlaylistsPageLanding.tsx
@@ -1,10 +1,13 @@
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import React, { useEffect, useRef, useState } from 'react';
 import { getMyPlaylists } from '../../apis/playlists';
+import { IconCard } from '../../components/cards/IconCard';
 import { InternalPlaylistCardList } from '../../components/cards/lists/PlaylistCardList';
 import { ProviderCard } from '../../components/cards/ProviderCard';
 import { LoadingSpinner } from '../../components/icons/LoadingSpinner';
 import { GeneralContent } from '../../components/layout/GeneralContent';
 import { Grid } from '../../components/layout/Grid';
+import { CreatePlaylistPopup } from '../../components/popup/popups/CreatePlaylistPopup';
 import { TopHeading } from '../../components/structure/TopHeading';
 import { useAppSelector } from '../../store/helper';
 import { InternalPlaylist, MediaProvider, UserTokenDTO } from '../../types';
@@ -12,9 +15,10 @@ import './PlaylistsPageLanding.scss';
 
 export function PlaylistsPageLanding() {
   const [playlists, setPlaylists] = useState<InternalPlaylist[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
   const userToken = useAppSelector((state) => state.auth.token);
   const isMounted = useRef(false);
-  const [loading, setLoading] = useState(false);
 
   const updatePlaylists = (
     newUserToken: UserTokenDTO | undefined,
@@ -58,10 +62,16 @@ export function PlaylistsPageLanding() {
           <ProviderCard provider={provider} key={provider} />
         ))}
       </Grid>
-      <TopHeading subheading="Playlists">All Playlists</TopHeading>
+      <TopHeading
+        subheading="Playlists"
+        right={<IconCard icon={faPlus} onClick={() => setShowPopup(true)}>New Playlist</IconCard>}
+      >
+        All Playlists
+      </TopHeading>
       {loading
         ? (<LoadingSpinner size="5x" />)
         : (<InternalPlaylistCardList playlists={playlists} areLinks />)}
+      <CreatePlaylistPopup visible={showPopup} onClose={() => setShowPopup(false)} />
     </GeneralContent>
   );
 }
