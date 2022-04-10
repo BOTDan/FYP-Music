ea0b1194d5d21d4e20e49533ba9a3f9b43ed2282
Dan Wells <dantheradarman@gmail.com>
2022-04-10 22:33:04 +0100

    📝 Expanded ProviderCard out to IconCard for reusability

--- client/src/components/cards/ProviderCard.tsx
+++ client/src/components/cards/ProviderCard.tsx
@@ -5,15 +5,32 @@ import { Link } from 'react-router-dom';
 import { mediaProviderPrettyPrint } from '../../helper';
 import { MediaProvider } from '../../types';
 import { classes, ProviderIcon } from '../icons/ProviderIcon';
+import { IconCard } from './IconCard';
 import './ProviderCard.scss';
 
 export interface ProviderCardProps {
   provider: MediaProvider;
 }
 
+/**
+ * Creates an icon card using the icon and brnading of the given media provider
+ * @param props
+ * @returns An icon card with provider icon+text
+ */
 export function ProviderCard({ provider }: ProviderCardProps) {
   const classList = ['ProviderCard'];
   classList.push(classes[provider]);
+
+  return (
+    <IconCard
+      className={classList.join(' ')}
+      iconContent={(<ProviderIcon provider={provider} />)}
+      to={`./${provider}`}
+      showArrow
+    >
+      {mediaProviderPrettyPrint(provider)}
+    </IconCard>
+  );
   return (
     <Link className={classList.join(' ')} to={`./${provider}`}>
       <div className="ProviderCard__Left">
