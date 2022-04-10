ef6a9830326f74891c442f22b0d52aeb3426fc64
Dan Wells <dantheradarman@gmail.com>
2022-04-10 22:30:38 +0100

    📝 TopHeading more functionality

--- client/src/components/structure/TopHeading.tsx
+++ client/src/components/structure/TopHeading.tsx
@@ -5,11 +5,15 @@ import './TopHeading.scss';
 export interface TopHeadingProps extends PropsWithChildren<{}> {
   subheading: string | ReactNode | undefined;
   image?: string;
+  imageFallback?: string;
+  right?: ReactNode;
 }
 
-export function TopHeading({ children, subheading, image }: TopHeadingProps) {
-  const imageComponent = image
-    ? <SquareImage className="TopHeading__Image" src={image} />
+export function TopHeading({
+  children, subheading, image, right, imageFallback,
+}: TopHeadingProps) {
+  const imageComponent = (image || imageFallback)
+    ? <SquareImage className="TopHeading__Image" src={image} fallbackSrc={imageFallback} />
     : undefined;
   return (
     <div className="TopHeading">
@@ -18,10 +22,17 @@ export function TopHeading({ children, subheading, image }: TopHeadingProps) {
         {subheading && <p className="TopHeading__Subheading">{ subheading }</p>}
         <h1 className="TopHeading__Heading">{ children }</h1>
       </div>
+      {right && (
+      <div className="TopHeading__Right">
+        {right}
+      </div>
+      )}
     </div>
   );
 }
 
 TopHeading.defaultProps = {
   image: undefined,
+  imageFallback: undefined,
+  right: undefined,
 };
