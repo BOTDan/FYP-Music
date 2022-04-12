6d275ffa280a145577da41b884cc28e485e2fe5d
Dan Wells <dantheradarman@gmail.com>
2022-04-12 04:09:07 +0100

    🆕 String inputs can now have a clear button

--- client/src/components/input/StringInput.tsx
+++ client/src/components/input/StringInput.tsx
@@ -1,21 +1,47 @@
+import { faClose } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
+import { Button } from './Button';
 import { GenericInput, GenericInputProps } from './GenericInput';
 
 interface StringInputProps extends Omit<GenericInputProps, 'type'> {
   onChange?(value: string): void;
   value: string;
+  showClearButton?: boolean;
 }
 
-export function StringInput({ onChange, ...remainingProps }: StringInputProps) {
+export function StringInput({
+  onChange, showClearButton, after, ...remainingProps
+}: StringInputProps) {
   function handleOnChange(value: string | number) {
     if (onChange) {
       onChange(value.toString());
     }
   }
 
-  return (<GenericInput type="text" onChange={handleOnChange} {...remainingProps} />);
+  const afterElems = (showClearButton)
+    ? (
+      <>
+        <Button
+          leftIcon={faClose}
+          onClick={() => handleOnChange('')}
+          bland
+        />
+        {after}
+      </>
+    )
+    : after;
+
+  return (
+    <GenericInput
+      type="text"
+      onChange={handleOnChange}
+      after={afterElems}
+      {...remainingProps}
+    />
+  );
 }
 
 StringInput.defaultProps = {
   onChange: () => {},
+  showClearButton: false,
 };
