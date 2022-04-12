0b4b44c01645174f4cde9c97c255186b3b8f31e0
Dan Wells <dantheradarman@gmail.com>
2022-04-12 03:16:47 +0100

    📝 Added icons and disabled functionality to buttons

--- client/src/components/input/Button.tsx
+++ client/src/components/input/Button.tsx
@@ -1,3 +1,5 @@
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, {
   ButtonHTMLAttributes, MouseEvent, PropsWithChildren, useCallback,
 } from 'react';
@@ -5,24 +7,33 @@ import './Button.scss';
 
 interface ButtonProps extends PropsWithChildren<Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'onClick'>> {
   onClick?: (event?: MouseEvent) => void;
+  leftIcon?: IconDefinition;
+  rightIcon?: IconDefinition;
 }
 
 export function Button({
-  children, onClick, className, ...remainingProps
+  children, onClick, className, leftIcon, rightIcon, disabled, ...remainingProps
 }: ButtonProps) {
   const handleOnClick = useCallback((event: MouseEvent<HTMLButtonElement>) => {
-    if (onClick) {
+    if (onClick && !disabled) {
       onClick(event);
     }
-  }, []);
+  }, [onClick, disabled]);
+
+  const leftIconComponent = (leftIcon) ? <FontAwesomeIcon icon={leftIcon} /> : (null);
+  const rightIconComponent = (rightIcon) ? <FontAwesomeIcon icon={rightIcon} /> : (null);
 
   return (
-    <button className={`Button ${className ?? ''}`} type="button" onClick={handleOnClick} {...remainingProps}>
+    <button className={`Button ${className ?? ''}`} type="button" onClick={handleOnClick} disabled={disabled} {...remainingProps}>
+      {leftIconComponent}
       {children}
+      {rightIconComponent}
     </button>
   );
 }
 
 Button.defaultProps = {
   onClick: () => {},
+  leftIcon: undefined,
+  rightIcon: undefined,
 };
