329a74aa12571884f939bfc90f3dc984112a6a70
Dan Wells <dantheradarman@gmail.com>
2022-04-13 03:17:39 +0100

    🆕 Playlist MultiSelect component

---
+++ client/src/components/input/advanced/PlaylistMultiSelect.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import { ExternalPlaylist, InternalPlaylist } from '../../../types';
+import { SquareImage } from '../../structure/SquareImage';
+import { MultiSelect, MultiSelectOption } from '../MultiSelect';
+import './PlaylistMultiSelect.scss';
+
+type PlaylistType = InternalPlaylist | ExternalPlaylist;
+
+export interface PlaylistMultiSelectProps<T extends PlaylistType> {
+  playlists: T[];
+  checked: T[];
+  onCheck(playlist: T): void;
+  onUncheck(playlist: T): void;
+}
+
+export function PlaylistMultiSelect<T extends PlaylistType>({
+  playlists, checked, onCheck: onAdded, onUncheck: onRemoved,
+}: PlaylistMultiSelectProps<T>) {
+  const [checkedInternal, setCheckedInternal] = useState<MultiSelectOption<T>[]>([]);
+  const [optionsInternal, setOptionsInternal] = useState<MultiSelectOption<T>[]>([]);
+
+  function handleOnChange(option: MultiSelectOption<T>, isChecked: boolean) {
+    if (isChecked) {
+      onAdded(option.value);
+    } else {
+      onRemoved(option.value);
+    }
+  }
+
+  useEffect(() => {
+    const checkedNames = checked.map((o) => (o as InternalPlaylist).id
+      ?? (o as ExternalPlaylist).providerId);
+    setCheckedInternal(
+      optionsInternal.filter(
+        (option) => checkedNames.includes(
+          (option.value as InternalPlaylist).id
+          ?? (option.value as ExternalPlaylist).providerId,
+        ),
+      ),
+    );
+  }, [checked]);
+
+  useEffect(() => {
+    setOptionsInternal(playlists.map((playlist) => ({
+      content: (
+        <div className="PlaylistMultiSelect__Label">
+          <SquareImage
+            className="PlaylistMultiSelect__Label__Image"
+            src={playlist.image}
+            fallbackSrc="/assets/img/playlist_placeholder.png"
+          />
+          <p className="PlaylistMultiSelect__Label__Text">
+            {playlist.name}
+          </p>
+        </div>
+      ),
+      name: (playlist as InternalPlaylist).id ?? (playlist as ExternalPlaylist).providerId,
+      value: playlist,
+    })));
+  }, [playlists]);
+
+  return (
+    <MultiSelect
+      className="PlaylistMultiSelect"
+      rightAlign
+      options={optionsInternal}
+      checked={checkedInternal}
+      onChange={handleOnChange}
+    />
+  );
+}
