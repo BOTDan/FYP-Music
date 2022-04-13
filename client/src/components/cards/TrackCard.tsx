6feeff5273bd3d0aff3f91538adf3b7570a565ff
Dan Wells <dantheradarman@gmail.com>
2022-04-13 20:31:26 +0100

    🆕 TrackCard now has small and inactive options

--- client/src/components/cards/TrackCard.tsx
+++ client/src/components/cards/TrackCard.tsx
@@ -1,30 +1,64 @@
-import React from 'react';
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
 import { formatTime } from '../../helper';
-import { useAppDispatch } from '../../store/helper';
-import { updateTrackToAdd } from '../../store/reducers/playlists';
 import { ExternalTrack } from '../../types';
 import { ProviderIcon } from '../icons/ProviderIcon';
 import { Button } from '../input/Button';
+// eslint-disable-next-line import/no-cycle
+import { TrackOptionsPopup } from '../popup/popups/TrackOptionsPopup';
 import './TrackCard.scss';
 
 export interface TrackCardProps {
   track: ExternalTrack;
   number?: number | undefined;
+  small?: boolean;
+  inactive?: boolean;
 }
 
-export function TrackCard({ track, number }: TrackCardProps) {
-  const dispatch = useAppDispatch();
+export function TrackCard({
+  track, number, small, inactive,
+}: TrackCardProps) {
+  const [optionsPopupVisible, setOptionsPopupVisible] = useState(false);
   const artists = track.artists.map((artist) => artist.name).join(', ');
 
-  function test() {
-    dispatch(updateTrackToAdd(track));
-  }
+  const numberElement = (
+    <span className="TrackCard__Number">
+      {number}
+    </span>
+  );
+
+  const durationElement = (
+    <span className="TrackCard__Duration">
+      {formatTime(track.duration)}
+    </span>
+  );
+
+  const buttonElement = (
+    <Button
+      className="TrackCard__Actions"
+      onClick={() => setOptionsPopupVisible(true)}
+      leftIcon={faEllipsisVertical}
+      bland
+    />
+  );
+
+  const optionsPopup = optionsPopupVisible
+    ? (
+      <TrackOptionsPopup
+        visible={optionsPopupVisible}
+        onClose={() => setOptionsPopupVisible(false)}
+        track={track}
+      />
+    )
+    : (null);
+
+  const classList = ['TrackCard'];
+  if (small) { classList.push('Small'); }
+  if (inactive) { classList.push('Inactive'); }
 
   return (
-    <div className="TrackCard">
-      <span className="TrackCard__Number">
-        { number }
-      </span>
+    <div className={classList.join(' ')}>
+      {!small && numberElement}
       <span className="TrackCard__Provider">
         <ProviderIcon provider={track.provider} />
       </span>
@@ -35,14 +69,15 @@ export function TrackCard({ track, number }: TrackCardProps) {
         <p className="TrackCard__Name">{ track.name }</p>
         <p className="TrackCard__Artists">{ artists }</p>
       </span>
-      <span className="TrackCard__Duration">
-        { formatTime(track.duration) }
-      </span>
-      <Button onClick={test}>+</Button>
+      {!small && durationElement}
+      {!inactive && buttonElement}
+      {optionsPopup}
     </div>
   );
 }
 
 TrackCard.defaultProps = {
   number: undefined,
+  small: false,
+  inactive: false,
 };
