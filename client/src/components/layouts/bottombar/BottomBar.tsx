aad9364ba49301c63dafb125365b4032173dad01
Dan Wells <dantheradarman@gmail.com>
2022-04-23 20:17:50 +0100

    🆕 Added PlaybackBar to BottomBar

--- client/src/components/layouts/bottombar/BottomBar.tsx
+++ client/src/components/layouts/bottombar/BottomBar.tsx
@@ -1,5 +1,7 @@
 import { faHome, faList } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
+import { GeneralContent } from '../../layout/GeneralContent';
+import { PlaybackBar } from '../../playback/PlaybackBar';
 import './BottomBar.scss';
 import { BottomBarLink } from './BottomBarLink';
 
@@ -9,8 +11,13 @@ import { BottomBarLink } from './BottomBarLink';
 export function BottomBar() {
   return (
     <div className="BottomBar">
-      <BottomBarLink name="Home" icon={faHome} to="/" />
-      <BottomBarLink name="Playlists" icon={faList} to="/playlists" />
+      <GeneralContent>
+        <PlaybackBar />
+        <div className="BottomBar__Links">
+          <BottomBarLink name="Home" icon={faHome} to="/" />
+          <BottomBarLink name="Playlists" icon={faList} to="/playlists" />
+        </div>
+      </GeneralContent>
     </div>
   );
 }
