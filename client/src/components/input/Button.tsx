1b471e0c65fa4772ebad85a14bc5e4199488d9e9
Dan Wells <dantheradarman@gmail.com>
2022-04-29 06:29:26 +0100

    🆕 Loading and Link button support

--- client/src/components/input/Button.tsx
+++ client/src/components/input/Button.tsx
@@ -3,26 +3,39 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, {
   ButtonHTMLAttributes, MouseEvent, PropsWithChildren, useCallback,
 } from 'react';
+import { To, useNavigate } from 'react-router';
+import { LoadingSpinner } from '../icons/LoadingSpinner';
 import './Button.scss';
 
 interface ButtonProps extends PropsWithChildren<Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'onClick'>> {
   onClick?: (event?: MouseEvent) => void;
+  to?: To;
   leftIcon?: IconDefinition;
   rightIcon?: IconDefinition;
   primary?: boolean;
   bland?: boolean;
+  loading?: boolean;
 }
 
 export function Button({
-  children, onClick, className, leftIcon, rightIcon, disabled, primary, bland, ...remainingProps
+  children, onClick, className, leftIcon, rightIcon, disabled, primary, bland, loading, to,
+  ...remainingProps
 }: ButtonProps) {
+  const navigate = useNavigate();
+
   const handleOnClick = useCallback((event: MouseEvent<HTMLButtonElement>) => {
+    if (to) {
+      navigate(to);
+    }
     if (onClick && !disabled) {
       onClick(event);
     }
   }, [onClick, disabled]);
 
-  const leftIconComponent = (leftIcon) ? <FontAwesomeIcon icon={leftIcon} fixedWidth /> : (null);
+  let leftIconComponent = (leftIcon) ? <FontAwesomeIcon icon={leftIcon} fixedWidth /> : (null);
+  if (loading) {
+    leftIconComponent = <LoadingSpinner inline />;
+  }
   const rightIconComponent = (rightIcon) ? <FontAwesomeIcon icon={rightIcon} fixedWidth /> : (null);
 
   const classList = ['Button'];
@@ -34,7 +47,13 @@ export function Button({
   }
 
   return (
-    <button className={classList.join(' ')} type="button" onClick={handleOnClick} disabled={disabled} {...remainingProps}>
+    <button
+      className={classList.join(' ')}
+      type="button"
+      onClick={handleOnClick}
+      disabled={disabled || loading}
+      {...remainingProps}
+    >
       {leftIconComponent}
       {children}
       {rightIconComponent}
@@ -48,4 +67,6 @@ Button.defaultProps = {
   rightIcon: undefined,
   primary: false,
   bland: false,
+  loading: false,
+  to: undefined,
 };
