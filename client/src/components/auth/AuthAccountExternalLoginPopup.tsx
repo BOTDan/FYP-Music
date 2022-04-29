d2273411ac4cf484d0888b1bbdb8ce7d91956e44
Dan Wells <dantheradarman@gmail.com>
2022-04-29 06:28:38 +0100

    📝 Changed component names, added linking support

--- client/src/components/auth/AuthAccountExternalLoginPopup.tsx
+++ client/src/components/auth/AuthAccountExternalLoginPopup.tsx
@@ -6,14 +6,15 @@ export interface LoginPopupProps {
   visible: boolean;
   onClose?: () => void;
   onCode: (code: string, state: string, provider: AuthProvider) => void;
+  link?: boolean;
 }
 
 /**
  * Creates an external login popup and listens for a success
  * @param props The props for the login popup
  */
-export function LoginPopup({
-  provider, visible, onClose, onCode,
+export function AuthAccountExternalLoginPopup({
+  provider, visible, onClose, onCode, link,
 }: LoginPopupProps) {
   const [externalWindow, setExternalWindow] = useState<Window | null>(null);
   const interval = useRef<number>();
@@ -28,7 +29,8 @@ export function LoginPopup({
         const left = window.screenX + (window.outerWidth - width) / 2;
         const top = window.screenY + (window.outerHeight - height) / 2;
         const title = 'Log In';
-        const url = `/auth/${provider}/login`;
+
+        const url = `/auth/${provider}/${link ? 'link' : 'login'}`;
 
         const win = window.open(url, title, `width=${width},height=${height},left=${left},top=${top}`);
         setExternalWindow(win);
@@ -43,7 +45,7 @@ export function LoginPopup({
           const curUrl = externalWindow?.location.href;
           const params = new URL(curUrl).searchParams;
           const code = params.get('code');
-          const state = params.get('state') ?? 'login';
+          const state = params.get('state') ?? (link ? 'link' : 'login');
           if (!code) { return; }
           onCode(code, state, provider);
           window.clearInterval(interval.current);
@@ -65,3 +67,7 @@ export function LoginPopup({
 
   return (null);
 }
+
+AuthAccountExternalLoginPopup.defaultProps = {
+  link: false,
+};
